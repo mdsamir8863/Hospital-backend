8ed5b3daaf78abd0b7edf6a964eba3de15f54b7e
mdsamir8863 <mentoonsdeveloper.ansari@gmail.com>
2024-08-03 23:13:57 +0530

    cores updated

--- backend/app.js
+++ backend/app.js
@@ -16,14 +16,16 @@ import userRouter from './router/userRouter.js';
 const app = express();
 config({ path: "./config/config.env" });
 
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  cors()
 );
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
